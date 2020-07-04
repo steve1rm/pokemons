88962f8f34b06d5931efa14ec88d19ec43e94f06
steve <stevekitkat@gmail.com>
2020-07-05 01:17:22 +0700

    Setting up adapter

--- app/src/main/java/me/androidbox/pokemon/presentation/screens/PokemonListFragment.kt
+++ app/src/main/java/me/androidbox/pokemon/presentation/screens/PokemonListFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import me.androidbox.pokemon.databinding.FragmentPokemonListBinding
 import me.androidbox.pokemon.di.ProvideApplicationComponent.getApplicationComponent
 import me.androidbox.pokemon.di.modules.PokemonModule
 import me.androidbox.pokemon.presentation.adapters.PokemonAdapter
-import timber.log.Timber
 import javax.inject.Inject
 
 class PokemonListFragment : Fragment() {
@@ -29,13 +31,19 @@ class PokemonListFragment : Fragment() {
         getApplicationComponent(requireActivity())
             .add(PokemonModule())
             .inject(this@PokemonListFragment)
-
-        Timber.d(TAG, "Injection success")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bindings = FragmentPokemonListBinding.inflate(inflater, container, false)
 
+        setupAdapter()
+
         return bindings.root
+    }
+
+    private fun setupAdapter() {
+        bindings.rvPokemons.adapter = pokemonAdapter
+        bindings.rvPokemons.layoutManager = LinearLayoutManager(requireContext(), VERTICAL, false)
+        bindings.rvPokemons.addItemDecoration(DividerItemDecoration(requireContext(), VERTICAL))
     }
 }
