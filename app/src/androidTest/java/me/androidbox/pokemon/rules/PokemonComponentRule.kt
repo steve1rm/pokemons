4fe1d23785d653e21c12812044c7f1cfc0c249be
steve <stevekitkat@gmail.com>
2020-07-08 13:39:12 +0700

    adds rule

---
+++ app/src/androidTest/java/me/androidbox/pokemon/rules/PokemonComponentRule.kt
@@ -0,0 +1,33 @@
+package me.androidbox.pokemon.rules
+
+import android.content.Context
+import me.androidbox.pokemon.di.*
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class PokemonComponentRule(private val context: Context): TestRule {
+
+    private var testComponent: TestComponent? = null
+
+    private fun setupTestDependencies() {
+        val application = context.applicationContext as PokemonApplication
+
+        val testComponent = DaggerTestComponent
+            .builder()
+            .testApplicationModule(TestApplicationModule(application))
+            .testNetworkModule(TestNetworkModule())
+            .build()
+
+        application.applicationComponent = testComponent
+    }
+
+    override fun apply(base: Statement?, description: Description?): Statement {
+        return object : Statement() {
+            override fun evaluate() {
+                setupTestDependencies()
+                base?.evaluate()
+            }
+        }
+    }
+}
