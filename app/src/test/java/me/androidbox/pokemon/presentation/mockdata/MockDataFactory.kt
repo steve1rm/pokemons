720fdf895098ddae6388dfdfd310e19030273eff
Steve <steve@kitkat@gmail.com>
2021-12-22 23:21:58 +0700

    Fixes broken tests needs to create new mock factory to create PokemonModels from the data layer

--- app/src/test/java/me/androidbox/pokemon/presentation/mockdata/MockDataFactory.kt
+++ app/src/test/java/me/androidbox/pokemon/presentation/mockdata/MockDataFactory.kt
@@ -1,5 +1,8 @@
 package me.androidbox.pokemon.presentation.mockdata
 
+import me.androidbox.domain.entity.PokemonEntity
+import me.androidbox.domain.entity.PokemonListEntity
+import me.androidbox.domain.entity.SpriteEntity
 import me.androidbox.pokemon.domain.entity.PokemonList
 import me.androidbox.pokemon.domain.entity.Pokemon
 import me.androidbox.pokemon.domain.entity.Sprite
@@ -7,8 +10,8 @@ import java.util.*
 
 object MockDataFactory {
 
-    fun createListOfPokemons(count: Int): List<Pokemon> {
-        val pokemonList = mutableListOf<Pokemon>()
+    fun createListOfPokemons(count: Int): List<PokemonEntity> {
+        val pokemonList = mutableListOf<PokemonEntity>()
 
         repeat(count) {
             pokemonList.add(createPokemon())
@@ -17,21 +20,21 @@ object MockDataFactory {
         return pokemonList.toList()
     }
 
-    fun createPokemonList(count: Int): PokemonList {
+    fun createPokemonList(count: Int): PokemonListEntity {
         return createListOfPokemons(10)
             .run {
-                PokemonList(this)
+                PokemonListEntity(this)
             }
     }
 
-    fun createPokemon(): Pokemon {
-        return Pokemon(
+    fun createPokemon(): PokemonEntity {
+        return PokemonEntity(
             UUID.randomUUID().toString(),
             (10..100).shuffled().first().toFloat(),
             (10..100).shuffled().first().toFloat(),
             (10..100).shuffled().first(),
             UUID.randomUUID().toString(),
-            Sprite(UUID.randomUUID().toString())
+            SpriteEntity(UUID.randomUUID().toString())
         )
     }
 }
