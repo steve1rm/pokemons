0c72767382cba0ab7a07692eb8c7713dd04afade
Steve <steve@kitkat@gmail.com>
2021-12-27 13:36:32 +0700

    Adding gradle appDistribution for distributing release

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,11 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:_")
         classpath("org.jacoco:org.jacoco.core:_")
-
+        classpath("com.google.gms:google-services:${Versions.googleServicesVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlinVersion}")
         val navVersion = "2.3.5"
         classpath (AndroidX.navigation.safeArgsGradlePlugin)
+        classpath("com.google.firebase:firebase-appdistribution-gradle:2.2.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
