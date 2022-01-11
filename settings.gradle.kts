4ed200b2139ea2286848897904eb3d8d6adada1f
Steve <steve@kitkat@gmail.com>
2022-01-11 23:04:50 +0700

    Updates version files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,8 @@ rootProject.name = "Pokemon"
 
 plugins {
     id("de.fayard.refreshVersions") version "0.23.0"
+////                            # available:"0.30.0"
+////                            # available:"0.30.1"
+////                            # available:"0.30.2"
 }
 include(":domain")
